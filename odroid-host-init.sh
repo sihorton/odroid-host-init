0c9a04cc215cfae9170d64d9ad668e857a501fcd
sihorton <sihorton@gmail.com>
2014-12-03 23:59:06 +0100

    Removed excess script so it now only installs the latest docker.io

--- odroid-host-init.sh
+++ odroid-host-init.sh
@@ -20,22 +20,3 @@ wget http://ftp.de.debian.org/debian/pool/main/d/docker.io/docker.io_1.3.2~dfsg1
 
 sudo dpkg -i docker.io_1.3.2~dfsg1-1_armhf.deb init-system-helpers_1.22_all.deb libdevmapper1.02.1_1.02.90-2_armhf.deb dmsetup_1.02.90-2_armhf.deb 
 
-#output the host id.
-echo "odroid server" >> /etc/profile.d/host.sh
-chmod +x /etc/profile.d/host.sh
-
-#I prefer dropbear so swap out openssh
-sudo apt-get remove openssh-server
-sudo apt-get install -y dropbear
-
-
-cd ..
-rm -rf deb_temp/
-#for odroid only
-wget http://builder.mdrjr.net/tools/kernel-update.sh
-
-#messages for the user
-echo "script complete"
-echo "edit /etc/profile.d/host.sh to identify this new host when you login."
-echo "edit /etc/samba/smb.conf to add any samba shares on this host."
-echo "run kernel-update.sh to update the odroid kernel"
