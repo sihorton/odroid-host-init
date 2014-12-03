366a268b0f3562052b192e8fffe9a719f37640ad
sihorton <sihorton@gmail.com>
2014-12-03 23:09:12 +0100

    Update odroid-host-init.sh

--- odroid-host-init.sh
+++ odroid-host-init.sh
@@ -1,1 +1,19 @@
 sudo apt-get install docker.io
+
+#docker.io in standard repos does not work on arm so download and install required packages from debian instead.
+
+#install a newer perl
+wget http://ftp.de.debian.org/debian/pool/main/p/perl/perl-modules_5.20.1-3_all.deb
+wget http://ftp.de.debian.org/debian/pool/main/p/perl/perl-base_5.20.1-3_armhf.deb
+wget http://ftp.de.debian.org/debian/pool/main/p/perl/perl_5.20.1-3_armhf.deb
+
+sudo dpkg -i perl_5.20.1-3_armhf.deb perl-base_5.20.1-3_armhf.deb perl-modules_5.20.1-3_all.deb
+
+#install docker.io dependancies
+wget http://ftp.de.debian.org/debian/pool/main/l/lvm2/libdevmapper1.02.1_1.02.90-2_armhf.deb
+wget http://ftp.de.debian.org/debian/pool/main/l/lvm2/dmsetup_1.02.90-2_armhf.deb
+wget http://ftp.de.debian.org/debian/pool/main/i/init-system-helpers/init-system-helpers_1.22_all.deb
+
+wget http://ftp.de.debian.org/debian/pool/main/d/docker.io/docker.io_1.3.2~dfsg1-1_armhf.deb
+
+sudo dpkg -i docker.io_1.3.2~dfsg1-1_armhf.deb init-system-helpers_1.22_all.deb libdevmapper1.02.1_1.02.90-2_armhf.deb dmsetup_1.02.90-2_armhf.deb 
